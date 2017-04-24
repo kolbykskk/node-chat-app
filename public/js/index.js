b12da8bb3aede032297047b740766632ef217ede
Kolby <kolbykalafut@gmail.com>
2017-04-24 01:36:54 -0400

    test

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Kolby',
-    text: 'Bey! What\'s up?'
-  });
 });
 
 socket.on('disconnect', function () {
